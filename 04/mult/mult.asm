a3ef7408f6756f36e6d4d2f613120f35893a7dea
Tzuan1020 <s111010517@student.nqu.edu.tw>
2022-12-14 15:00:58 +0800

    finish

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,22 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+//此程式是參考影片所完成 https://www.youtube.com/watch?v=aC-IeMoZN4M
+@0
+D=A
+@2
+M=D
+@0
+D=M
+@16
+D;JLE
+@1
+D=M
+@2
+M=D+M
+@0
+M=M-1
+@4
+0;JMP
+@16
+0;JMP
